b3c04c7f91e70d4e9ea4ea6dd18263d153701875
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-08-09 16:41:29 +0530

    Create printDups.cpp

---
+++ Day-5/printDups.cpp
@@ -0,0 +1,11 @@
+void printDups(string str)
+{   
+  unordered_map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        count[str[i]]++;
+    }
+  
+    for (auto it : count) {
+        if (it.second > 1)cout << it.first << ", count = " << it.second<< "\n";
+    }
+}
