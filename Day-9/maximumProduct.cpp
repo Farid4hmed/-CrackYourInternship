16b6f8aeeb87cfa5ac539c54a12533d54c9c63da
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-08-13 15:16:05 +0530

    Create maximumProduct.cpp

---
+++ Day-9/maximumProduct.cpp
@@ -0,0 +1,4 @@
+int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return max(nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3], nums[0]*nums[1]*nums[nums.size()-1]);
+    }
