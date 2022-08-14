4e36c1d754902ecc461384055a5b3eaa8aa4ea54
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-08-14 21:41:04 +0530

    Create ceilSearch.cpp

---
+++ Sorting and Searching (Easy-Medium)/ceilSearch.cpp
@@ -0,0 +1,13 @@
+int ceilSearch(int arr[], int low, int high, int x){
+    int i;
+    if(x <= arr[low])
+        return low;
+    for(i = low; i < high; i++){
+        if(arr[i] == x)
+        return i;
+     
+        if(arr[i] < x && arr[i+1] >= x)
+        return i+1;
+    }    
+    return -1;
+}
