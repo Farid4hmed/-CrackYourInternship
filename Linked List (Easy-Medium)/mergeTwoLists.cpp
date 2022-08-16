c823f154ac54272b796d0b1374fac208f3150dca
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-08-16 13:39:45 +0530

    Create mergeTwoLists.cpp

---
+++ Linked List (Easy-Medium)/mergeTwoLists.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        if(list1 == NULL)return list2;
+        if(list2 == NULL)return list1;
+        ListNode* head;
+        if(list1->val < list2->val){
+            head = list1;
+            list1 = list1->next;
+        }
+        else{
+            head = list2;
+            list2 = list2->next;
+        }
+        ListNode* curr = head;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        if(!list1)curr->next = list2;
+        else curr->next = list1;
+        return head;
+    }
+};
