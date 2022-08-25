da128775517293ec9018d5b63f34d8d8cdde05d9
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-08-25 13:55:40 +0530

    Create stockSpanner.cpp

---
+++ Stacks & Queues (Easy-Medium)/stockSpanner.cpp
@@ -0,0 +1,16 @@
+class StockSpanner {
+public:
+    StockSpanner() {
+    }
+    
+    stack<pair<int, int>> st;
+    int next(int price) {
+            int res = 1;
+        while (!st.empty() && st.top().first <= price) {
+            res += st.top().second;
+            st.pop();
+        }
+        st.push({price, res});
+        return res;
+    }
+};
