d5514d98379d341b347ce169263cfdc222881667
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-08-31 15:24:10 +0530

    Create lowestCommonAncestorBT.cpp

--- Trees (Easy-Medium)/lowestCommonAncestorBT.cpp
+++ Trees (Easy-Medium)/lowestCommonAncestorBT.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL)return NULL;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        if(root == NULL || root == p || root == q)return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p , q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if(left != NULL && right != NULL)return root;
-        else {
-            if(left == NULL && right == NULL){
-                if(root->val == p->val)return p;
-                else if(root->val == q->val)return q;
-                else return NULL;
-            }
-            else {
-                if(left!=NULL){
-                    if(root->val == p->val && root->val != left->val || root->val == q->val && root->val != left->val)return root;
-                    else return left;
-                }
-                if(right!=NULL){
-                    if(root->val == p->val && root->val != right->val || root->val == q->val && root->val != right->val)return root;
-                    else return right;
-                }
-                }
-            }
-            return NULL;
+        
+            if(left == NULL)return right;
+            if(right == NULL)return left;
+        
         }
 };
