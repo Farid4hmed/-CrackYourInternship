770583f3d03bd607d1f1f222dee7dc2061fc8af4
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-09-02 00:33:10 +0530

    Create post_order.cpp

---
+++ Trees (Easy-Medium)/post_order.cpp
@@ -0,0 +1,16 @@
+Node* helper(int pre[],int n,int bound,int &i)
+   {
+       if(i==n || pre[i]>bound)
+       return NULL;
+       Node* root=newNode(pre[i]);
+       i++;
+       root->left=helper(pre,n,root->data,i);
+       root->right=helper(pre,n,bound,i);
+       return root;
+   }
+   Node* post_order(int pre[], int n)
+   {
+       //code here
+       int i=0;
+       return helper(pre,n,INT_MAX,i);
+   }
