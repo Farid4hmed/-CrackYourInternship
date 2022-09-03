8e9652cdbbf603053070d4cd0c81aa5a13612e2f
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-09-03 22:08:01 +0530

    Create primMST.cpp

---
+++ Graph (Easy-Medium)/primMST.cpp
@@ -0,0 +1,25 @@
+int primMST(int graph[V][V]) 
+{ 
+
+	int key[V];int res=0; 
+	fill(key,key+V,INT_MAX);
+	bool mSet[V]; key[0]=0;
+
+	for (int count = 0; count < V ; count++) 
+	{ 
+		int u = -1; 
+
+		for(int i=0;i<V;i++)
+		    if(!mSet[i]&&(u==-1||key[i]<key[u]))
+		        u=i;
+		mSet[u] = true; 
+		res+=key[u];
+
+		
+		for (int v = 0; v < V; v++) 
+
+			if (graph[u][v]!=0 && mSet[v] == false) 
+				key[v] = min(key[v],graph[u][v]); 
+	} 
+    return res;
+} 
