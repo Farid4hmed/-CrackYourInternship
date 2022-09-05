f2340d07bd0849acff822a66e2cfe711500e479c
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-09-05 20:31:57 +0530

    Create kthSmallest.cpp

---
+++ Priority Queues (Easy-Medium)/kthSmallest.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                pq.push(matrix[i][j]);
+            }
+        }
+        k--;
+        while(k--){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
