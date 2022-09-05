a23425fa56919132468d208c43012b09f4f4782d
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-09-05 19:49:18 +0530

    Create nthUglyNumber.cpp

---
+++ Priority Queues (Easy-Medium)/nthUglyNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        set<long> st;
+        st.insert(1);
+        long num = 1;
+        for(int i=0;i<n;i++){
+            num = *st.begin();
+            st.erase(num);
+            st.insert(num * 2);
+            st.insert(num * 3);
+            st.insert(num * 5);
+        }
+        return num;
+    }
+};
